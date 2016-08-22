868c07c076b5e2dca9619dc5abf247c8caddf9e9
Leon Chou <bleepbloopsify@gmail.com>
2016-08-22 14:03:54 -0400

    connect

--- test/accounts.js
+++ test/accounts.js
@@ -2,7 +2,8 @@ var chai = require('chai');
 var should = chai.should();
 
 var mongoose = require('mongoose');
-mongoose.createConnection(process.env.TEST_DB_URL || "mongodb://localhost:27017/framework-testing");
+mongoose.connect(process.env.TEST_DB_URL || "mongodb://localhost:27017/framework-testing");
+mongoose.Promise = global.Promise;
 
 var Account = require('../models/account').Model;
 
@@ -14,6 +15,15 @@ describe('Account Model', function(){
   after(function(done){
     Account.collection.drop();
     done();
+  });
+
+  it('creates a record', function(done){
+    newAccount = new Account({
+      'local':{
+        'email':'hello@hello.com',
+      }
+    });
+    newAccount.save(done);
   });
 
 
